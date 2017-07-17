90327050f61f4bed5cae797413651dbcce887361
JayWenChao <1217960903@qq.com>
2017-07-17 14:50:43 +0800

    idea  更改测试

--- src/main/java/cn/hy/com/SpringbootCloudApplication.java
+++ src/main/java/cn/hy/com/SpringbootCloudApplication.java
@@ -1,5 +1,6 @@
 package cn.hy.com;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +16,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SpringbootCloudApplication {
 
+	private static final Logger logger = Logger.getLogger(SpringbootCloudApplication.class);
 
 	@RequestMapping(value = "/thymplate",method = RequestMethod.GET)
 	public String getHello(){
-
+        logger.info("为啥会这样");
 		System.out.println("-----------");
 		return "word";
 
