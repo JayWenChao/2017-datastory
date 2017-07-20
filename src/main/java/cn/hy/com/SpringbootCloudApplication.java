720096578ccd4a3a853e4ae619225a6ab998c856
JayWenChao <1217960903@qq.com>
2017-07-20 19:31:06 +0800

    定时调度

--- src/main/java/cn/hy/com/SpringbootCloudApplication.java
+++ src/main/java/cn/hy/com/SpringbootCloudApplication.java
@@ -1,19 +1,26 @@
 package cn.hy.com;
 
-import org.apache.log4j.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
-
+/**
+ * 使用Mybaits的ORM映射框架，采用Ehcache缓存机制
+ * 使用注释@EnableCacheing
+ * 开启定时调度机制，使用注解@EnableScheduling
+ */
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-@Controller
+@ComponentScan
+@EnableScheduling
 public class SpringbootCloudApplication {
+
 
 
 	public static void main(String[] args) {
