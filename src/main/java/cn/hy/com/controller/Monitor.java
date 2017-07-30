322f34df955bf3c32c3161271e810187a1d3f08b
JayWenChao <1217960903@qq.com>
2017-07-30 11:09:21 +0800

    修改反射异常问题

--- src/main/java/cn/hy/com/controller/Monitor.java
+++ src/main/java/cn/hy/com/controller/Monitor.java
@@ -3,6 +3,8 @@ package cn.hy.com.controller;
 import cn.hy.com.entity.Student;
 import cn.hy.com.service.RedisServerImpl;
 import cn.hy.com.service.StudentService;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -33,13 +40,14 @@ public class Monitor {
     private RedisServerImpl redisServer;
 
     @RequestMapping(value = "/detail/{id}",method = RequestMethod.GET)
-    public Object getJobDatail(@PathVariable("id") Integer id, HttpServletRequest request){
+    public Object getJobDatail(@PathVariable("id") Integer id, HttpServletRequest request) throws NoSuchFieldException, IllegalAccessException, InstantiationException, InvocationTargetException {
 
 
 
         System.out.println(host+"-----主机------");
         logger.info("数据访问");
         Student student = studentService.selectSingle(id);
+        
         Student st = new Student();
         st.setId(8);
         st.setPasswd("cmt");
@@ -48,14 +56,17 @@ public class Monitor {
         redisServer.put("he",st,-1);
         Set<String> keys = redisServer.getAllKeys();
         keys.stream().forEach(e ->{System.out.println(e);});
-        List<Student> allValue = redisServer.getAllValue();
-        for(int i=0;i<allValue.size();i++){
+        List<Student> allValue =  redisServer.getAllValue();
+        String str = allValue.toString();
+        System.out.println(str.getClass().getName());
+        JSONArray parse = (JSONArray) JSONObject.parse(str);
+        for (int i = 0; i <parse.size() ; i++) {
 
-            System.out.println(allValue.get(i));
-            String s = allValue.get(i).toString();
-            System.out.println(s);
+            Student sd = JSONObject.parseObject(parse.get(i).toString(), Student.class);
+            System.out.println(sd.getUsername());
         }
-        System.out.println(allValue);
+        System.out.println(parse.getClass().getName());
+        //System.out.println(json);
         long size = redisServer.size();
         System.out.println("数据的长度是:"+size);
         return student;
