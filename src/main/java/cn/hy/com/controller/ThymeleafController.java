f9e1d51b5068c3f9bc59d57aecbe4c654f859e9c
JayWenChao <1217960903@qq.com>
2017-07-30 22:05:53 +0800

    实现springboot-springsecurity基本功能

--- src/main/java/cn/hy/com/controller/ThymeleafController.java
+++ src/main/java/cn/hy/com/controller/ThymeleafController.java
@@ -1,14 +1,22 @@
 package cn.hy.com.controller;
 
+import cn.hy.com.entity.Student;
+import cn.hy.com.service.StudentService;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
- *
+ * springsecurity安全验证后，成功调转的页面，所需的用户名
+ * 从springsecurity中
  * Created by Administrator on 2017/7/17.
  */
 @Controller
@@ -16,13 +24,15 @@ public class ThymeleafController {
 
     private static final  Logger logger = Logger.getLogger(ThymeleafController.class);
 
+    @Autowired
+    private StudentService studentService;
     /**
      * 使用Thymeleaf的坑，找不到404，在springboot-1.4版本以上
      * 一般需要定义Thymeleaf的版本问题
      * @param model
      * @return
      */
-    @RequestMapping(value = "/data",method = RequestMethod.GET)
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String getHello(Model model){
 
         logger.info("---------------------");
@@ -30,5 +40,34 @@ public class ThymeleafController {
         return "word";
 
     }
+
+
+    /**
+     * 成功调转的页面
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/dashboard",method = RequestMethod.GET)
+    public String  TryLogin(Model model) {
+
+
+        //获取安全上下文环境
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        Object principal = authentication.getPrincipal();
+        Object details = authentication.getDetails();
+        System.out.println(details);
+        System.out.println(principal.getClass().getName().equals(UserDetails.class));
+        if(principal instanceof UserDetails){
+
+            String username = ((UserDetails) principal).getUsername();
+            model.addAttribute("username",username);
+        }else{
+
+           return principal.toString();
+        }
+        return "vue";
+    }
+
 
 }
